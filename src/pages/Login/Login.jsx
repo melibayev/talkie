180d237a516ca0d96c57b3b9e60676d31634e70a
Elbek <113505483+melibayev@users.noreply.github.com>
2024-01-14 00:59:00 +0500

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,13 +2,59 @@ import { Fragment, useState } from "react"
 import { NavLink } from "react-router-dom";
 import { BiShow, BiHide } from "react-icons/bi";
 import styles from './Login.module.scss'
+import loginValidation from "../../validation/loginValidation";
+import { request } from '../../server/request'
 
 import GIF from '../../assets/login/bg.gif'
 import LOGO from '../../assets/login/logo.png'
+import { useForm } from "react-hook-form";
+import { Button } from "antd";
 
 
 const Login = () => {
 const [ passwordShow, setPasswordShow ] = useState(false)
+const [loading, setLoading] = useState([]);
+const [ haveError, setHaveError ] = useState(false)
+const [ error, setError ] = useState('')
+const {
+    register, 
+    handleSubmit,
+} = useForm()
+const submit = async (data) => {
+    try {
+      await loginValidation.validate(data);
+      setHaveError(false);
+      enterLoading(0)
+  
+      try {
+        enterLoading(0)
+        let res = await request.post('Auth/login', data);
+        console.log(res);
+      } catch (error) {
+        stopLoading(0)
+        setHaveError(true);
+        setError('Your account is not found. Check your username and password');
+      }
+    } catch (validationError) {
+      stopLoading(0)
+      setHaveError(true);
+      setError(validationError.message);
+    }
+  };
+const enterLoading = (index) => {
+    setLoading((prevLoadings) => {
+      const newLoadings = [...prevLoadings];
+      newLoadings[index] = true;
+      return newLoadings;
+    });
+}
+const stopLoading = (index) => {
+    setLoading((prevLoadings) => {
+      const newLoadings = [...prevLoadings];
+      newLoadings[index] = false;
+      return newLoadings;
+    });
+}
   return (
     <Fragment>
         <section id={styles.login}>
@@ -17,10 +63,10 @@ const [ passwordShow, setPasswordShow ] = useState(false)
                     <div className={styles['form-info']}>
                         <img src={LOGO} alt="logo" />                        
                         <h1>Nice to see you again</h1>
-                        <form>
-                            <input type="text" placeholder="Username" required />
+                        <form onSubmit={handleSubmit(submit)}>
+                            <input type="text" placeholder="Username" {...register('username', { required: true })} />
                             <div className={styles['password']}>
-                                <input type={passwordShow ? 'text' : 'password'} placeholder="Password" required />
+                                <input type={passwordShow ? 'text' : 'password'} placeholder="Password" {...register('password', { required: true })}/>
                                 <div onClick={() => setPasswordShow(!passwordShow)}>
                                     { passwordShow ? <BiHide /> : <BiShow /> } 
                                 </div>
@@ -28,18 +74,21 @@ const [ passwordShow, setPasswordShow ] = useState(false)
                             <div className={styles['form-info-extra']}>
                                 <div className={styles['form-info-extra-remember-me']}>
                                     <div>Remember Me</div>
-                                    <div class={styles['checkbox-wrapper']}>
-                                        <input class={styles['substituted']} type="checkbox" aria-hidden="true" />
+                                    <div className={styles['checkbox-wrapper']}>
+                                        <input className={styles['substituted']} type="checkbox" aria-hidden="true" />
                                     </div>
                                 </div>
                                 <div>Forgot Password</div>
                             </div>
-                            <button>Sign in</button>
+                            <Button htmlType="submit" loading={loading[0]} className={styles['form-info-button']}>
+                                Sign in
+                            </Button>
                         </form>
                         <div className={styles['form-info-sign-up-message']}>
                             <p>Don't have an account?</p>
                             <NavLink to={'/register'}>Sign up now</NavLink>
                         </div>
+                        <div className={styles['form-info-error-message']}>{haveError ? error : ''}</div>
                         <div className={styles['form-info-credits']}>
                             © Talkie 2024
                         </div>
